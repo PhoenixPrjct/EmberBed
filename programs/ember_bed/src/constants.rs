fc14427730c99fc4dd3d60a6540df9ed439272d5
Peter Roto <peterdroto@gmail.com>
2023-11-05 21:05:23 -0600

    removed secret key from code -- account was already compromised so nbd

--- programs/ember_bed/src/constants.rs
+++ programs/ember_bed/src/constants.rs
@@ -5,4 +5,4 @@ pub const FIRE_COLLECTION_NAME: &str = "EmberBed";
 pub const FIRE_SYMBOL: &str = "$FIRE";
 pub const PHOENIX_WALLET: &str = "BhsVi2SQBvvddpxEYWt3Xnhu4udeeKwkS7uHGTDPhSmn";
 // "E9NxULjZAxU4j1NYkDRN2YVpmixoyLX3fd1SsWRooPLB";
-pub const PHOENIX_ADMIN_WALLET: &str = "PrJctdsdT314Ny5AH7aUB11pC9Gxidr12uNTAYr9GzN";
+pub const PHOENIX_ADMIN_WALLET: &str = "F1R3hniEv24tNuu7XcSsYSgp2azCGwx7ignGXhcr62vG";
