3b74e745a7708d96af1b610ceeeb425c0e5fcd09
Peter Roto <peterdroto@gmail.com>
2024-01-05 16:12:59 -0600

    ran build locally successfully

---
+++ app/dist/spa/assets/QSpace.08e50f5c.js
@@ -0,0 +1,1 @@
+import{g as a,a as e}from"./index.3ba0a9f2.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
--- app/server/server.ts
+++ app/server/server.ts
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-const distPath = path.join(__dirname, ".../dist/spa")
+const distPath = path.join(__dirname, "..", "/dist/spa")
 // if (!process.env.IS_DEV) {
 app.use(express.static(distPath));
 app.use(routes);
