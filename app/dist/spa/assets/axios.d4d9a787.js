9342f1c6ece6ffec2f6077a9037cd3e2cab028dd
Peter Roto <peterdroto@gmail.com>
2024-01-05 15:51:12 -0600

    increased HEAP

---
+++ app/dist/spa/assets/axios.d4d9a787.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.3ba0a9f2.js";import{a}from"./index.6bbe8a48.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
